70c3ffffa5c366cea92c3879a62585e970828061
Jaime Martin <jaime.martin@campuscamara.es>
2025-10-22 12:37:24 +0200

    ExamenFlutter2

--- examenflutter/lib/main.dart
+++ examenflutter/lib/main.dart
@@ -2,24 +2,24 @@ class Persona {
   String nombre = "";
   int edad = 0;
 
-  //Construntores
+  //constructor nombre edad
   Persona.nombreedad(String nombrem, int edadd){
     this.nombre = nombrem;
     this.edad = edadd;
   }
-
+  //Constructor default
   Persona(){
     this.nombre = "Desconocido";
     this.edad = 0;
   }
-  
+  //Constructor de fabrica
   factory Persona.fabrica(int edad){
     if(edad <= 0){
       throw ("La edad es menor a 0");
     }
       return Persona.nombreedad("Desconocido", edad);
   }
-
+  //Metodo mostrar infor
   void mostrarInfo(){
     print("nombre: $nombre, edad: $edad");
   }
@@ -29,7 +29,7 @@ class Persona {
 class Empleado extends Persona {
   String puesto = "";
   double salario = 0.0;
-
+  //Constructor
   Empleado(String nombr,int eda, String puest, double salari){
     this.nombre = nombr;
     this.edad = eda;
@@ -37,19 +37,19 @@ class Empleado extends Persona {
     this.salario = salari;
     
   }
-  
+  //Mostrar Detalles
   void mostrarDetalles(){
     super.mostrarInfo();
     print("puesto: $puesto, salario: $salario");
   }
-
+  //Metodo sobreescrito
   @override
   void mostrarInfo(){
     super.mostrarInfo();
     String salario = tipoSalario();
     print("puesto: $puesto, salario: $salario, Tipo de Salario: $salario");
   }
-
+  //Tipo salario
   String tipoSalario(){
     if(salario <1200){
       return "salario bajo";
@@ -63,12 +63,12 @@ class Empleado extends Persona {
 
 }
 void main(){
-
+    //Empleado solo
     Empleado jaime = Empleado("Jaime", 20, "chofer", 900);
     jaime.mostrarInfo();
 
 
-  /*
+    //Lista empleados
     List<Empleado> listaEmpleados = [];
     Empleado juan = Empleado("juan", 29, "secretario", 2000);
     listaEmpleados.add(juan);
@@ -80,11 +80,12 @@ void main(){
     listaEmpleados.add(rey);
     Empleado ernesto = Empleado("ernesto", 2, "secretario", 80000);
     listaEmpleados.add(ernesto);
+    //Recorrer lista
     for(var lista in listaEmpleados){
       lista.mostrarDetalles();
       String salario = lista.tipoSalario();
       print(salario);
     }
-    */
+    
 
   }
